3acfc0e3729755490457ab0c765b7962ede79dda
Rohit Negi <rohitnegi263139@gmail.com>
2024-03-13 18:23:54 +0530

    create header and footer component

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = ( logo ) => {
+  return( `<div class="header">
+            <img src=${logo} />
+            <div>
+                <p>FAQ's</p>
+                <p>About Us</p>
+            </div>
+        </div>`)
+}
+
+export default Header
---
+++ components/footer.js
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return  `<div class="footer">
+            <ul>
+                <li>Copyright © Tradify</li>
+                <li class="footer-text">FAQ's</li>
+                <li class="footer-text">About Us</li>
+            </ul>
+        </div>`
+}
+
+export default Footer
